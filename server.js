ba1c37f50e523f4e0c95e5ce087c20316cb012de
katyjayne <katyjayneblank@gmail.com>
2024-03-11 14:02:08 -0400

    fixed some stuff before it got out of hand

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const pg = require("pg");
+
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_notes_categories_db"
+);
+
+const init = async () => {
+  console.log("connecting to database");
+  await client.connect();
+  console.log("connected to database");
+  let SQL = `
+    DROP TABLE IF EXISTS notes;
+    DROP TABLE IF EXISTS categories;
+    CREATE TABLE categories(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL
+    );
+    CREATE TABLE notes(
+      id SERIAL PRIMARY KEY,
+      txt VARCHAR(100) NOT NULL,
+      ranking INTEGER NOT NULL DEFAULT 5,
+      created_at TIMESTAMP DEFAULT now(),
+      updated_at TIMESTAMP DEFAULT now(),
+      category_id INTEGER REFERENCES categories(id) NOT NULL
+    );
+  `;
+};
+
+init();
