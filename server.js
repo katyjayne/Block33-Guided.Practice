14dc60c75cf4a72a08be1b44678dfd0cc987f74b
katyjayne <katyjayneblank@gmail.com>
2024-03-11 16:14:08 -0400

    got routes working

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const pg = require("pg");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_notes_categories_db"
 );
+
+app.use(morgan("dev"));
+
+app.get("/api/notes", async (req, res, next) => {
+  try {
+    const SQL = `
+      SELECT * 
+      FROM notes;
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/categories", async (req, res, next) => {
+  try {
+    const SQL = `
+      SELECT * 
+      FROM categories;
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 const init = async () => {
   console.log("connecting to database");
@@ -41,6 +70,9 @@ const init = async () => {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`listening on port ${port}`);
+    console.log("curl commands to test application");
+    console.log(`curl localhost:${port}/api/notes`);
+    console.log(`curl localhost:${port}/api/categories`);
   });
 };
 
